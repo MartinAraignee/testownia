d0a295032c1953c2d500586398ef8adedae848a7
Martin <martinaraignee@hotmail.com>
2017-10-08 11:35:04 +0100

    testing new branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :posts do
+    resources :comments, only: [:new, :index, :create]
+    resources :votes, only: [:create, :destroy]
+  end
+
+
+
+  devise_for :users
   #get 'welcome/index'
 
-  root 'welcome#index'
+
+
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
